8dc82f84d3b7b0805ca71812c4c6fe30c218fd5e
Shekhar Shekhar <sshek2@sapient.com>
2018-01-14 16:35:18 +0530

    notification services uses https

--- services/notificationservice/bin/run.js
+++ services/notificationservice/bin/run.js
@@ -1,15 +1,27 @@
 'use strict'
 const path =  require('path');
-const http = require('http');
+const https = require('https');
 const express = require('express');
 const socketIO = require('socket.io');
 const config = require('./../../common/config');
 const NotificationService = require('./../notificationservice');
+const fs = require('fs');
 
 const publicPath = path.join(__dirname, './../../../public');
 const port = process.env.NOTIFICATION_SERVICE_PORT || 3040;
 var app = express();
-var server = http.createServer(app);
+
+// openssl genrsa -out privatekey.pem 2048
+// openssl req -new -key privatekey.pem -out certrequest.csr
+// openssl x509 -req -in certrequest.csr -signkey privatekey.pem -out certificate.pem
+const options = {
+  key  : fs.readFileSync(path.join(__dirname, '../certs/privatekey.pem')),
+  cert : fs.readFileSync(path.join(__dirname, '../certs/certificate.pem')),
+};
+
+// Create our HTTPS server listening on port 3000.
+var server = https.createServer(options, app);
+
 var io = socketIO(server);
 app.use(express.static(publicPath));
 
